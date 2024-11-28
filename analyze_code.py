7fb975ca66fca1ee845b4ce3e135f88284258ed0
Charles Chibueze <charlesukpaka@ymail.com>
2024-11-28 13:53:48 +0100

    more changes

--- analyze_code.py
+++ analyze_code.py
@@ -43,7 +43,7 @@ def analyze_code_chunk(chunk, chat_model):
     )
     prompt = prompt_template.format(code=chunk)
     response = chat_model.invoke(prompt)
-    return response.content
+    return response
 
 def analyze_large_file(file_content):
     
